68d6dac180444e9d40553fdbcfbf92454fbc39da
Siddhant Bhardwaj <bhardwajsiddhant03@gmail.com>
2020-11-15 22:40:12 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyB9Yru7eFqwodW7LOyfXCea7iIXSahGQwM",
+    authDomain: "fireflix-417f8.firebaseapp.com",
+    databaseURL: "https://fireflix-417f8.firebaseio.com",
+    projectId: "fireflix-417f8",
+    storageBucket: "fireflix-417f8.appspot.com",
+    messagingSenderId: "637031088940",
+    appId: "1:637031088940:web:0181dcdc5bd81313d66289",
+    measurementId: "G-W7218RY4DZ"
+});
+
